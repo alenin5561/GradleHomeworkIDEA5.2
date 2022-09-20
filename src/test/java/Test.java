75fc3ad2005ba77786148a29ea51a2fd0fc51a0c
Аленин Андрей <aleninmailbox@gmail.com>
2022-09-20 11:22:33 +0300

    tests

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,12 +1,10 @@
-import io.restassured.specification.RequestSpecification;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 
 import static com.codeborne.selenide.Selenide.open;
-import static data.DataGenerator.*;
 import static data.DataGenerator.Registration.getRegisteredUser;
 import static data.DataGenerator.Registration.getUser;
+import static data.DataGenerator.*;
 import static io.restassured.RestAssured.given;
 
 public class Test {
@@ -22,11 +20,12 @@ public class Test {
             var registeredUser = getRegisteredUser("active");
             given() // "дано"
                     .spec(requestSpec) // указываем, какую спецификацию используем
-                    .body(registeredUser) // передаём в теле объект, который будет преобразован в JSON
+                    .body(new RegistrationDto("vasya", "password", "active")) // передаём в теле объект, который будет преобразован в JSON
                     .when() // "когда"
                     .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
                     .then() // "тогда ожидаем"
-                    .statusCode(200); // код 200 OK
+                    .statusCode(200); // код 200 ... 500
+
             // TODO: добавить логику теста, в рамках которого будет выполнена попытка входа в личный кабинет с учётными
             //  данными зарегистрированного активного пользователя, для заполнения полей формы используйте
             //  пользователя registeredUser
@@ -36,6 +35,13 @@ public class Test {
         @DisplayName("Should get error message if login with not registered user")
         void shouldGetErrorIfNotRegisteredUser() {
             var notRegisteredUser = getUser("active");
+            given() // "дано"
+                    .spec(requestSpec) // указываем, какую спецификацию используем
+                    .body(notRegisteredUser) // передаём в теле объект, который будет преобразован в JSON
+                    .when() // "когда"
+                    .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
+                    .then() // "тогда ожидаем"
+                    .statusCode(200); // код 200 ... 500
             // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет
             //  незарегистрированного пользователя, для заполнения полей формы используйте пользователя notRegisteredUser
         }
@@ -44,6 +50,13 @@ public class Test {
         @DisplayName("Should get error message if login with blocked registered user")
         void shouldGetErrorIfBlockedUser() {
             var blockedUser = getRegisteredUser("blocked");
+           given() // "дано"
+                   .spec(requestSpec) // указываем, какую спецификацию используем
+                   .body(blockedUser) // передаём в теле объект, который будет преобразован в JSON
+                   .when() // "когда"
+                   .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
+                   .then() // "тогда ожидаем"
+                   .statusCode(500); // код 200 ... 500
             // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет,
             //  заблокированного пользователя, для заполнения полей формы используйте пользователя blockedUser
         }
@@ -53,6 +66,13 @@ public class Test {
         void shouldGetErrorIfWrongLogin() {
             var registeredUser = getRegisteredUser("active");
             var wrongLogin = getRandomLogin();
+            given() // "дано"
+                    .spec(requestSpec) // указываем, какую спецификацию используем
+                    .body(new RegistrationDto(wrongLogin, registeredUser, "active")) // передаём в теле объект, который будет преобразован в JSON
+                    .when() // "когда"
+                    .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
+                    .then() // "тогда ожидаем"
+                    .statusCode(200); // код 200 ... 500
             // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет с неверным
             //  логином, для заполнения поля формы "Логин" используйте переменную wrongLogin,
             //  "Пароль" - пользователя registeredUser
@@ -63,6 +83,13 @@ public class Test {
         void shouldGetErrorIfWrongPassword() {
             var registeredUser = getRegisteredUser("active");
             var wrongPassword = getRandomPassword();
+            given() // "дано"
+                    .spec(requestSpec) // указываем, какую спецификацию используем
+                    .body(new RegistrationDto(registeredUser, wrongPassword, "active")) // передаём в теле объект, который будет преобразован в JSON
+                    .when() // "когда"
+                    .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
+                    .then() // "тогда ожидаем"
+                    .statusCode(200); // код 200 ... 500
             // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет с неверным
             //  паролем, для заполнения поля формы "Логин" используйте пользователя registeredUser,
             //  "Пароль" - переменную wrongPassword
